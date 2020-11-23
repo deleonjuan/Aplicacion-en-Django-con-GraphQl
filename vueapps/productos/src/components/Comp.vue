fd04ac4eea53c360add9df82bb7b91226aaef30b
Joel Castillo <jcastillo1999jecm@gmail.com>
2020-11-23 11:44:51 -0600

    commit varios

---
+++ vueapps/productos/src/components/Comp.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <div>
+
+    <p>Hello World</p>
+
+  </div>
+
+</template>
+
+<script>
+  export default{
+    name: 'CompanyData',
+    data(){
+      return {}
+    }
+  }
+</script>
+
+<style scoped>
+</style>
