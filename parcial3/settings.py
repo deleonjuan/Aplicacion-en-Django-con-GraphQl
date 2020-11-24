90151f42bf531a9be78635be0c7097beacd47dfa
Joel Castillo <jcastillo1999jecm@gmail.com>
2020-11-23 19:06:44 -0600

    Implementacion al proyecto todo el frontend con solo vista de todos los productos que existen

--- parcial3/settings.py
+++ parcial3/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'graphene_django',
     'productos',
+    'corsheaders', 
 ]
 
 GRAPHENE = {
@@ -46,6 +47,7 @@ GRAPHENE = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8080'
+]
